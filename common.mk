4c48a70862e1591addc10eab6ec500f9f9cdab7e
Sonal Singh <sonal.singh.19993@gmail.com>
2021-05-29 13:08:10 +0000

    sm6250-common: evolution -> dot
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -291,7 +291,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evo
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
