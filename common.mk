65d682534b89db8a67df58b6e4fee9880b75395f
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-09 18:28:13 +0000

    sm6250-common: prebuilts: Drop GCamGOPrebuilt
    This reverts commit c9b035c88909c3b9a624fb81c2bc6146354470bd.
    
     * Also build Camera2 instead of Snap

--- common.mk
+++ common.mk
@@ -96,7 +96,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    GCamGOPrebuilt
+    Camera2
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
